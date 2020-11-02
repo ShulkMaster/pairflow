3fb9350700f70aee2923226735fc5e72add8be75
Yury Rivera <yrivera@applaudostudios.com>
2020-11-01 20:21:08 -0600

    moving to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import logo from './logo.svg';
 
 ReactDOM.render(
   <React.StrictMode>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
